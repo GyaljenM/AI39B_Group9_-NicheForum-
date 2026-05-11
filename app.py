1b496d6b434dc3741efb1a159fbdd94f691da5aa
Law1it <suyogtuladhar04@gmail.com>
2026-05-11 19:48:00 +0545

    hells kitchen

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Hello, World! This is my first flask website."
+    return "Hello, kim"
 
 if __name__ == "__main__":
     app.run(debug=True)

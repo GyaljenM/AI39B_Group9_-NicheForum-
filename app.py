8c95772a836b542ef8f0252eb3b07453d6545efa
unknown <lamagyalzen5@gmail.com>
2026-05-12 12:16:01 +0545

    added about , contact and 404 error

--- app.py
+++ app.py
@@ -4,7 +4,19 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Hello, World! This is my first flask website."
+    return "Welcome to the Home Page"
+
+@app.route("/about")
+def about():
+    return "This is the About Page"
+
+@app.route("/contact")
+def contact():
+    return "Contact us at [email protected]"
+
+@app.error_handler(404)
+def error(e):
+    return f"404 page not found please check it again!"
 
 if __name__ == "__main__":
     app.run(debug=True)

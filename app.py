7b7ac1cb95e9735726a48186f637267793eabe5d
unknown <lamagyalzen5@gmail.com>
2026-05-25 12:07:37 +0545

    fixed routing

--- app.py
+++ app.py
@@ -1,14 +1,6 @@
-from flask import Flask, render_template
+from app import create_app
 
-app = Flask(__name__, template_folder="app/templates", static_folder="app/static")
-
-@app.route("/")
-def home():
-    return render_template("home.html")
-
-@app.route("/index")
-def index():
-    return render_template("index.html")
+app = create_app()
 
 if __name__ == "__main__":
     app.run(debug=True)

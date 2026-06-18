ba9424ce5c0c62bc8cb2082fbfb1273f8735ac32
unknown <lamagyalzen5@gmail.com>
2026-06-18 16:03:46 +0545

    new changes

--- config.py
+++ config.py
@@ -2,7 +2,7 @@ SECRET_KEY = "random-secret-key"
 
 MYSQL_HOST = "localhost"
 MYSQL_USER = "root"
-MYSQL_PASSWORD = "hussdai@6787Lmao"
+MYSQL_PASSWORD = "4425148$uyoG"
 MYSQL_DB = "class_db"
 
 # Backwards-compatible alias: some modules use MYSQL_DATABASE
